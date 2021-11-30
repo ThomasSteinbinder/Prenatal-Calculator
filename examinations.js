3a034f0b949c87f49a1228004cd1d33e6b237753
Thomas Steinbinder <thomas.steinbinder@onlinehome.de>
2021-11-30 23:15:01 +0100

    Added NIPT exam

--- examinations.js
+++ examinations.js
@@ -20,7 +20,7 @@ export default [
     new Examination(
         "Chorionzottenbiopsie / CVS",
         13,
-        14,
+        15,
         "TODO",
         "https://de.praenatal-medizin.de/files/2021/09/Flyer_Praenatale-Diagnostik-aus-Chorionzotten-oder-Plazentazotten_12-2020.pdf",
         "#FA66A1"
@@ -40,5 +40,13 @@ export default [
         "TODO",
         "https://de.praenatal-medizin.de/files/2021/09/Flyer_Praenatale-Diagnostik-aus-Fruchtwasser_12-2020.pdf",
         "#FBACF8"
+    ),
+    new Examination(
+        "NIPT",
+        10,
+        40,
+        "",
+        "https://de.praenatal-medizin.de/diagnostik/genetik/nipt/",
+        "#FBB06A"
     )
 ]

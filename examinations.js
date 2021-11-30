76f1b104961de3219ae7c665fd5cdefbc488c653
Thomas Steinbinder <thomas.steinbinder@onlinehome.de>
2021-11-30 22:34:18 +0100

    Added color to examinations

--- examinations.js
+++ examinations.js
@@ -1,10 +1,11 @@
 class Examination {
-    constructor(name, fromWeek, toWeek, description, link) {
+    constructor(name, fromWeek, toWeek, description, link, color) {
         this.name = name;
         this.fromWeek = fromWeek;
         this.toWeek = toWeek;
         this.description = description;
         this.link = link;
+        this.color = color;
     }
 }
 export default [
@@ -13,28 +14,31 @@ export default [
         13,
         14,
         "TODO",
-        "https://de.praenatal-medizin.de/files/2021/09/Flyer_Einscha%CC%88tzung-maternaler-Risiken-im-ersten-Trimenon_12-2020.pdf"
+        "https://de.praenatal-medizin.de/files/2021/09/Flyer_Einscha%CC%88tzung-maternaler-Risiken-im-ersten-Trimenon_12-2020.pdf",
+        "#C9FCFD"
     ),
     new Examination(
         "Chorionzottenbiopsie / CVS",
         13,
         14,
         "TODO",
-        "https://de.praenatal-medizin.de/files/2021/09/Flyer_Praenatale-Diagnostik-aus-Chorionzotten-oder-Plazentazotten_12-2020.pdf"
+        "https://de.praenatal-medizin.de/files/2021/09/Flyer_Praenatale-Diagnostik-aus-Chorionzotten-oder-Plazentazotten_12-2020.pdf",
+        "#FA66A1"
     ),
     new Examination(
         "Fehlbildungsauschluss / FBA / Großer Ultraschall / Feindiagnostik",
         21,
         22,
         "TODO",
-        "https://de.praenatal-medizin.de/diagnostik/sonografie/ii-trimenon-fba/"
+        "https://de.praenatal-medizin.de/diagnostik/sonografie/ii-trimenon-fba/",
+        "#8D9EF7"
     ),
     new Examination(
         "Amniozentese",
         16,
         40,
         "TODO",
-        "https://de.praenatal-medizin.de/files/2021/09/Flyer_Praenatale-Diagnostik-aus-Fruchtwasser_12-2020.pdf"
+        "https://de.praenatal-medizin.de/files/2021/09/Flyer_Praenatale-Diagnostik-aus-Fruchtwasser_12-2020.pdf",
+        "#FBACF8"
     )
 ]
-
